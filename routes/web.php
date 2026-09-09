f7ac03bad16deaec8e15018e3aa0018525689ae7
Artur Vidal <arturvidalcontato@gmail.com>
2026-09-09 18:17:13 -0300

    feat: register route and create controller class Creates the route POST /register for sending user registration data. The controller class is also created as UserController.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,4 +16,12 @@ Route::controller(AuthController::class)
         Route::get('/logout', 'logout')->name('logout');
 
         Route::view('/admin', 'admin.home')->middleware('admin');
+    });
+
+Route::controller(UserController::class)
+    ->name('user.')
+    ->group(function() {
+
+        Route::post('/register', 'register')->name('register');
+
     });
