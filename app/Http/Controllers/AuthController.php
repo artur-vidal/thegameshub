2a48fd4654ddc5519dd2a346e18f5f3e674eb543
Artur Vidal <arturvidalcontato@gmail.com>
2026-09-09 15:02:53 -0300

    feat: implemented login/logout route methods

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function login(LoginRequest $request) {
+        $data = $request->validated();
+
+        if(!Auth::attempt($data)) {
+            return back()->withErrors('Credenciais inválidas.');
+        }
+
+        return view('admin.home');
+    }
+
+    public function logout() {
+        Auth::logout();
+        return view('auth.login');
+    }
 }
