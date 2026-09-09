a68f749fb887ad04e71670e9764845e60aac4fb0
Artur Vidal <arturvidalcontato@gmail.com>
2026-09-09 18:29:58 -0300

    feat: implemented user registration method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\User\UserStoreRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function register(UserStoreRequest $request) {
+        $data = $request->validated();
+        $user = User::create($data);
+        return redirect('/login');
+    }
 }
